bcdefb8ee1e20b22444a45fd1b874f1d02c9f3cc
SumitRoy03 <rsumit647@gmail.com>
2022-06-21 23:13:25 +0530

    connectDB

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+const mongoos = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoos.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected : ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit();
+  }
+};
+
+module.exports = connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
-const express = require("express")
-const chats = require("./data/data")
-const dotenv = require("dotenv")
-const app = express()
+const express = require("express");
+const chats = require("./data/data");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const app = express();
 
-dotenv.config()
+dotenv.config();
+connectDB();
+app.get("/", (req, res) => {
+  res.send("App is running on 5000!");
+});
 
-app.get('/', (req, res)=> {
-    res.send('App is running on 5000!')
-})
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
-app.get('/api/chat', (req,res)=> {
-    res.send(chats)
-})
+app.get("/api/chat/:id", (req, res) => {
+  // console.log(req.params.id)
+  const singlechat = chats.find((c) => c._id === req.params.id);
+  res.send(singlechat);
+});
 
-app.get('/api/chat/:id', (req,res)=>{
-    // console.log(req.params.id)
-    const singlechat = chats.find((c)=> c._id===req.params.id);
-    res.send(singlechat)
-}
-)
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server started on PORT : ${5000}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on PORT : ${5000}`));
